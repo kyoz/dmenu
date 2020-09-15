e4b27907fd165b4fd9b0cb298536a4b9c2f3acfc
Kyoz <banminkyoz@gmail.com>
2020-09-15 17:39:15 +0700

    update config.h

--- config.h
+++ config.h
@@ -24,4 +24,4 @@ static unsigned int lines      = 5;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 1;
+static const unsigned int border_width = 2;
